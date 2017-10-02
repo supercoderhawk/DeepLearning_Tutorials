1392684fd0427ffe3629990f6b49db11db9c14ed
supercoderhawk <supercoderhawk@gmail.com>
2017-10-02 21:40:54 +0800

    add test case

--- tests/test_list_utils.py
+++ tests/test_list_utils.py
@@ -11,39 +11,40 @@ class TestListUtils(TestCase):
     b = [[1, 2, 3], [], [4, [5]]]
     self.assertCountEqual(flatten(b), [1, 2, 3, 4, 5])
 
-    self.assertEqual(flatten([]),[])
+    self.assertEqual(flatten([]), [])
     try:
       flatten(1)
     except Exception as e:
-      self.assertEqual(e.args[0],'input is not a list')
+      self.assertEqual(e.args[0], 'input is not a list')
 
   def test_list_reshape(self):
-    a = [1,2,3,4,5,6,7,8]
-    a1 = [[1,2],[3,4],[5,6],[7,8]]
-    self.assertEqual(list_reshape(a,[4,2]),a1)
+    a = [1, 2, 3, 4, 5, 6, 7, 8]
+    a1 = [[1, 2], [3, 4], [5, 6], [7, 8]]
+    self.assertEqual(list_reshape(a, [4, 2]), a1)
+    self.assertEqual(list_reshape([1], [1, 1]), [[1]])
     try:
-      list_reshape(a, [4,1])
+      list_reshape(a, [4, 1])
     except Exception as e:
       self.assertEqual(e.args[0], 'element count is not equal to shape')
     try:
-      list_reshape(a, [-1,1])
+      list_reshape(a, [-1, 1])
     except Exception as e:
       self.assertEqual(e.args[0], 'dimension element is not positive integer')
     try:
-      list_reshape(1,[1,1])
+      list_reshape(1, [1, 1])
     except Exception as e:
-      self.assertEqual(e.args[0],'input content is not list type')
+      self.assertEqual(e.args[0], 'input content is not list type')
     try:
-      list_reshape(a,1)
+      list_reshape(a, 1)
     except Exception as e:
-      self.assertEqual(e.args[0],'input content is not list type')
+      self.assertEqual(e.args[0], 'input content is not list type')
 
   def test_list_size(self):
-    self.assertEqual(list_size([]),[])
-    self.assertEqual(list_size([1]),[1])
-    self.assertEqual(list_size([[1]]),[1,1])
+    self.assertEqual(list_size([]), [])
+    self.assertEqual(list_size([1]), [1])
+    self.assertEqual(list_size([[1]]), [1, 1])
     try:
-      list_size([1,[2]])
+      list_size([1, [2]])
     except Exception as e:
       self.assertEqual(e.args[0], 'input argument is not a matrix-liked list')
     try:
@@ -52,23 +53,23 @@ class TestListUtils(TestCase):
       self.assertEqual(e.args[0], 'input argument is not list')
 
   def test_list_add(self):
-    a = [[1,2],[3,4]]
-    b = [[11,12],[13,14]]
-    ab = [[12,14],[16,18]]
-    self.assertEqual(list_add(a,b),ab)
-    self.assertEqual(list_add([],[]),[])
-    self.assertEqual(list_add([1.0],[10]),[11])
+    a = [[1, 2], [3, 4]]
+    b = [[11, 12], [13, 14]]
+    ab = [[12, 14], [16, 18]]
+    self.assertEqual(list_add(a, b), ab)
+    self.assertEqual(list_add([], []), [])
+    self.assertEqual(list_add([1.0], [10]), [11])
     try:
-      list_add([1,2],[[3,4],[1,2]])
+      list_add([1, 2], [[3, 4], [1, 2]])
     except Exception as e:
-      self.assertEqual(e.args[0],'two arguments size length is not equal')
+      self.assertEqual(e.args[0], 'two arguments size length is not equal')
 
     try:
-      list_add([1,2],1)
+      list_add([1, 2], 1)
     except Exception as e:
       self.assertEqual(e.args[0], 'second argument is not a list')
 
     try:
-      list_add([(1,2)],[11])
+      list_add([(1, 2)], [11])
     except Exception as e:
       self.assertEqual(e.args[0], 'can only concatenate tuple (not "int") to tuple')
