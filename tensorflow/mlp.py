227519edd28faff4503d79f16365e118908ec418
supercoderhawk <supercoderhawk@gmail.com>
2017-12-24 17:45:35 +0800

    add files

---
+++ tensorflow/mlp.py
@@ -0,0 +1,14 @@
+#-*- coding: UTF-8 -*-
+"""使用tensorflow实现多层感知机"""
+import tensorflow as np
+
+class TfMLP(object):
+  def __init__(self):
+    pass
+
+  def variable(self,size):
+    return
+
+if __name__ == '__main__':
+  mlp = TfMLP()
+
